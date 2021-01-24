3502336e4e2406d40d7af98b97b9ea6ae4920ed5
indiu6 <hewas6@gmail.com>
2021-01-23 23:42:54 -0500

    done header

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
+      <Header title="Shopping List" />
       <Image
         source={{uri: 'https://randomuser.me/api/portraits/men/3.jpg'}}
         style={styles.img}
@@ -24,17 +17,14 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
+    paddingTop: 60,
   },
   img: {
     width: 100,
     height: 100,
     borderRadius: 100 / 2,
+    // alignContent: 'center',
+    // alignItems: 'center',
   },
 });
 
