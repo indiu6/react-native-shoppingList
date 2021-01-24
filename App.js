de3921430fd9c3691de9b04214786983ead39678
indiu6 <hewas6@gmail.com>
2021-01-23 23:16:51 -0500

    StyleSheet

--- App.js
+++ App.js
@@ -7,14 +7,35 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/3.jpg'}}
+        style={styles.img}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+});
 
 export default App;
